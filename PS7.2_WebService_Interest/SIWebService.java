f329bf2fa0e4dc965d5f37620a3e4e35416651c6
Shreeya Daga <66695381+ShreeyaDaga@users.noreply.github.com>
2026-05-08 09:07:17 +0530

    Netbeans Simple Interest

--- PS7.2_WebService_Interest/SIWebService.java
+++ PS7.2_WebService_Interest/SIWebService.java
@@ -1,45 +1,19 @@
 package org.si.calculator;
-
 import javax.jws.WebService;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.ejb.Stateless;
 
-/**
- * Simple Interest Calculator Web Service
- * Formula: SI = (P * R * T) / 100
- */
 @WebService(serviceName = "SIWebService")
 @Stateless()
 public class SIWebService {
 
-    /**
-     * Calculates Simple Interest
-     * @param principal  - Principal amount
-     * @param rate       - Rate of interest per annum
-     * @param time       - Time in years
-     * @return           - Simple Interest value
-     */
     @WebMethod(operationName = "calculateSI")
     public double calculateSI(
-            @WebParam(name = "principal") double principal,
-            @WebParam(name = "rate")      double rate,
-            @WebParam(name = "time")      double time) {
+        @WebParam(name = "principal") double principal,
+        @WebParam(name = "rate")      double rate,
+        @WebParam(name = "time")      double time) {
 
-        double si = (principal * rate * time) / 100;
-        return si;
-    }
-
-    /**
-     * Calculates Total Amount (Principal + SI)
-     */
-    @WebMethod(operationName = "calculateTotalAmount")
-    public double calculateTotalAmount(
-            @WebParam(name = "principal") double principal,
-            @WebParam(name = "rate")      double rate,
-            @WebParam(name = "time")      double time) {
-
-        double si = (principal * rate * time) / 100;
-        return principal + si;
+        return (principal * rate * time) / 100;
     }
 }
